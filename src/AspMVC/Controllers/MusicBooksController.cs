6baa8a3e98d0c1567eab4b797c516df209b2e77b
cristian-daniel88 <cristian.daniel.herrera@hotmail.com>
2023-02-18 23:23:38 +0000

    Updated Detail Actiton

--- src/AspMVC/Controllers/MusicBooksController.cs
+++ src/AspMVC/Controllers/MusicBooksController.cs
@@ -1,7 +1,25 @@
-﻿namespace AspMVC.Controllers
+﻿
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspMVC.Controllers
 {
     public class MusicBooksController : Controller
     {
+        public ActionResult Detail () 
+        {
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Friday) 
+            {
+                return Redirect("/");
+            }
 
+            return Content("Content result ! jjaja");
+            
+        }
+
+         public string index () 
+        {
+            return "Hola Music index";
+        }
     }
 }
